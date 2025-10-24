ce3f840321a4391f4df16fc0c3c34047714b2ea8
seveibar <seveibar@gmail.com>
2025-10-23 22:32:13 -0700

    some pipeline setup

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+import type { CircuitJsonUtilObjects } from "@tscircuit/circuit-json-util"
+import type { KicadPcb, KicadSch } from "kicadts"
+
+export interface ConverterContext {
+  db: CircuitJsonUtilObjects
+  kicadPcb: KicadPcb
+  kicadSch: KicadSch
+}
+
+export interface ConverterStage {}
