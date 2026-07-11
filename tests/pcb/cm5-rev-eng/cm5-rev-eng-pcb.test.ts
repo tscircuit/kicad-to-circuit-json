eee955a252863af5b9095f2f3a321e225d5287e0
mohan-bee <mohn08052006@gmail.com>
2026-07-11 09:18:15 +0530

    support cm5-rev-eng

---
+++ tests/pcb/cm5-rev-eng/cm5-rev-eng-pcb.test.ts
@@ -0,0 +1,11 @@
+import { test } from "bun:test"
+import { convertKicadPcbToSvgSnapshot } from "../../fixtures/svg-snapshot-test-utils"
+
+test("kicad-to-circuit-json: CM5RevEng SVG snapshot", () => {
+  convertKicadPcbToSvgSnapshot({
+    kicadPcbPath: "tests/assets/CM5RevEng.kicad_pcb",
+    kicadFileName: "CM5RevEng.kicad_pcb",
+    testPath: import.meta.path,
+    snapshotName: "cm5-rev-eng-circuit-json",
+  })
+})
